58002bd499ff2e781865b8dc10a00f983f7c9bc0
Marcos Toranzo Alfonso <m.toranzo@estudiantes.matcom.uh.cu>
2020-11-02 09:12:41 -0500

    Working around

--- routes/umUser/getFieldsCount.js
+++ routes/umUser/getFieldsCount.js
@@ -6,16 +6,12 @@ module.exports = {
   path: "/:field",
   handler: async (ctx) => {
     const { field } = ctx.request.params;
-    const {
-      data: { data },
-    } = await axios.get(
+    const { data } = await axios.get(
       "http://directoriounico.umcc.cu/api/getData.php?f=json&t=SIGENU"
     );
-
-    const ldapUser = await checkIfUserExists("gretell.umpierrez");
-    console.log(ldapUser);
+    ctx.assert(data, 404, "User information can not be fetched");
     const counter = {};
-    const filter = data.map((user) => {
+    data.map((user) => {
       if (!(user[field] in counter) && user.estado === "Activo") {
         counter[user[field]] = 0;
       } else counter[user[field]] += 1;
@@ -29,6 +25,7 @@ module.exports = {
       counter,
       counterArray,
     };
+
     ctx.status = 200;
   },
 };
